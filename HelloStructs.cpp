af269483b610ee598b72906c93afa67c4535d3de
ChocoChocobo <sharonovjr@yahoo.com>
2026-02-18 15:22:32 +0300

    18.02 mutex

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -1,30 +1,34 @@
 ﻿#include <string>
 #include <Windows.h>
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
 
-struct David
+using namespace std::chrono_literals;
+
+std::mutex mainMutex;
+std::vector<std::exception_ptr> exceptionsArray;
+
+void ThrowException()
 {
-    explicit David(int) {}
-    David(double) {}
-};
+    throw std::exception("Путхон.");
+}
 
-struct Evangelion
+// Функция, которая обрабатывает исключения параллельно
+void ThreadTryCatch() 
 {
-    David david1, david2;
-};
-
-struct Leon
-{
-    int x;
-    int y;
-};
-
-struct Platon {};
-
-struct CPlusPlus
-{
-    CPlusPlus(Platon){}
-};
+	try
+	{
+		ThrowException();
+	}
+	catch (const std::exception& mutexException)
+	{
+		std::lock_guard<std::mutex> lock(mainMutex);
+		exceptionsArray.push_back(std::current_exception());
+	}
+}
 
 int main()
 {
@@ -32,39 +36,44 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int x1 = 1; // Исторически сложившийся способ инициализации
-    int x2(2);
-    int x3{ 3 };
-    int x4 = { 4 }; // Практически идентичен int x3{ 3 }
-    auto x5{ 5 };
+	std::thread thr(ThreadTryCatch);
+	thr.join();
+	
+	std::this_thread::sleep_for(2000ms);
 
-    char goida[5] {'g', 'o', 'i', 'd', 'a'};
-
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << goida[i] << std::endl;
-    }
-
-    Leon leon = {  };
-    std::cout << leon.x << leon.y << std::endl;
-    if (leon.x)
-    {
-        std::cout << "Яблоко" << std::endl;
-    }
-    else
-    {
-        std::cout << "Тыблоко" << std::endl;
-    }
-
-    David david(5);
-    David david1 = 5;
-    
-    CPlusPlus(Platon());
+	for (auto& exception : exceptionsArray)
+	{
+		try
+		{
+			if (exception != nullptr)
+			{
+				std::rethrow_exception(exception);
+			}
+		}
+		catch (const std::exception& mutexException)
+		{
+			std::cout << "Исключение поймано: " << mutexException.what() << std::endl;
+		}
+	}
 
     system("pause");
 }
 
-// =========Практика=========
-// Написать структуру координаты (Coordinate), имеющую в качестве полей три int точки (x, y, z) и функцию SetCoordinates, которая бы задавала точкам новое значение. Конструктор explicit. 
-// В main() создать точки разными способам инициализации и вызвать функцию SetCoordinates у них.
-// Примечание: по необходимости дополнить структуру полями, если возникнут ограничения при инициализации
+//      Mutex - mutual exclusion, то есть взаимное исключение
+// Предоставляет функции для синхронизации одновременно выполняющихся потоков.
+// Когда какой-либо поток, принадлежащий процессу, становится владельцем объекта mutex, последний переводится в новое состояние, которое принято называть неотмеченным. Если задача освобождает объект mutex, то его состояние переходит в отмеченное.
+// Мьютексы являются элементарными двоичными семафорами с двумя состояниями (оимеченный объект и неотмеченный). Мьютекс отличается от семафора тем, что только владеющий им поток может его освобождать (переводить в отмеченное состояние)
+// Задачи: 
+// 1. Защита объект от доступа к нему других потоков
+// 2. В конкретный момент только один поток может владеть объектом под мьютексом.
+// 3. Если потоку нужен доступ к переменной под мьютексом, то этот поток заснет до тех пор, пока мьютекс не будет освобожден.
+// Мьютекс используется для защиты данных от изменений в результате асинхронных изменений, однако могут появляться проблемы взаимной блокировки процессов, если данным инструментом пользоваться неправильно.
+
+// У потока (thread) есть две полезные функции, позволяющие замедлять его выполнение:
+// std::this_thread::sleep_for() - блокирует выполнение текущего потока в течение установленного периода
+
+
+// std::this_thread::sleep_until() - блокирует выполнение текущего потока, пока не будет достигнут момент времени
+
+//			Практика
+// Поэкспериментировать с замедлением параллельного потока и попробовать заставить основной поток не успеть выполнить обработку исключений из другого потока
