b27ab29a1dddfa0cd75eee12986c9ed0c3a4e643
ChocoChocobo <sharonovjr@yahoo.com>
2026-02-27 12:04:59 +0300

    27.02 Декоратор

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -2,96 +2,58 @@
 #include <Windows.h>
 #include <iostream>
 
-// вкладываемая видеокарта в класс-контейнер компьютера
-class GPU
+// Класс с чистыми виртуальными функциями (интерфейс). Определяет поведение, которое изменяется с помощью декоратора
+class Component
 {
 public:
-    GPU() {}
-    GPU(std::string modelName)
-    {
-        this->modelName = modelName;
-    }
-    void PrintInfo()
-    {
-        std::cout << "Видокарта: " << modelName << std::endl;
-    }
-    void StartGPU()
-    {
-        std::cout << "Видеокарта начала свою работу!" << std::endl;
-    }
-private:
-    std::string modelName;
+    virtual ~Component(){}
+    virtual std::string DoSomething() = 0;
 };
 
-// вкладываемый процессор в класс-контейнер компьютера
-class CPU
+// Класс, реализующий поведение по умолчанию. У этого класса может быть несколько вариаций
+class PartComponent : public Component
 {
 public:
-    CPU() {}
-    CPU(std::string modelName)
+    std::string DoSomething() override
     {
-        this->modelName = modelName;
+        return "Defualt PartComponent";
     }
-    void PrintInfo()
-    {
-        std::cout << "Процессор: " << modelName << std::endl;
-    }
-    void StartCPU()
-    {
-        std::cout << "Процессор начал свою работу!" << std::endl;
-    }
-private:
-    std::string modelName;
 };
 
-// вкладываемая материнская плата в класс-контейнер компьютера
-class MotherBoard
+// Следует тому же интерфейсу, что и другие компоненты. Определяет обертку для всех конкретных декораторов
+class Decorator : public Component
 {
 public:
-    MotherBoard() {}
-    MotherBoard(std::string modelName)
+    Decorator(Component* component)
     {
-        this->modelName = modelName;
+        this->component = component;
     }
-    void PrintInfo()
+    std::string DoSomething() override
     {
-        std::cout << "Материнская плата: " << modelName << std::endl;
+        return component->DoSomething();
     }
-    void StartMotherBoard()
-    {
-        std::cout << "Материнская плата начала свою работу!" << std::endl;
-    }
-private:
-    std::string modelName;
+protected:
+    Component* component;
 };
 
-// Класс-контейнер для других объектов классов
-class Computer
+// Конкретные декораторы вызывают обернутый объект и изменяют его результат.
+// Декораторы могут вызывать родительскую реализацию операции, вместо того, чтобы вызвать обернутый объект напрямую
+class Minion : public Decorator
 {
 public:
-    Computer(std::string gpuModelName, std::string cpuModelName, std::string motherModelName)
+    Minion(Component* component) : Decorator(component) {}
+    std::string DoSomething() override
     {
-        gpu = new GPU(gpuModelName);
-        cpu = new CPU(cpuModelName);
-        motherBoard = new MotherBoard(motherModelName);
+        // При наследовании обращение к родителю идет через оператор области видимости, поскольку поля с объектом класса в текущем классе нет.
+        return "Minion сделал: " + Decorator::DoSomething();
     }
-    void StartComputer()
-    {
-        gpu->StartGPU();
-        cpu->StartCPU();
-        motherBoard->StartMotherBoard();
-    }
-    void PrintSpecifications()
-    {
-        gpu->PrintInfo();
-        cpu->PrintInfo();
-        motherBoard->PrintInfo();
-    }
-private:
-    GPU* gpu; // видеокарта
-    CPU* cpu; // процессор
-    MotherBoard* motherBoard; // материнская плата
 };
+
+// Функция, работающая  со всеми объектами, использующая интерфейс компонента.Функция независима от конкретных классов компонентов с которыми работает
+void GuideMinion(Component* component)
+{
+    std::cout << "Результат работы миньона: " << component->DoSomething();
+}
 
 int main()
 {
@@ -99,124 +61,25 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    Computer computer("GTX 1080", "intel core i7", "ASUS ROG");
-    computer.StartComputer();
-    std::cout << "========" << std::endl;
-    computer.PrintSpecifications();
+    // Поддержка простых компонентов:
+    Component* componentA = new PartComponent;
+    GuideMinion(componentA);
+    std::cout << std::endl << "==================" << std::endl;
+
+    // Поддержка декоративных компонентов:
+    Component* decoratorA = new Minion(componentA);
+    GuideMinion(decoratorA);
+    std::cout << std::endl << "==================" << std::endl;
+
+    delete componentA;
+    delete decoratorA;
 
     system("pause");
 }
 
-//          Практика
-// Написать класс дерева, имеющий два поля (кол-во веток, вид дерева). 
-// Написать класс леса с полем массива деревьев. Класс леса должен получать указатели на объекты деревьев извне через соответствующий метод.
-// Если класс леса удаляется, то деревья должны продолжать существовать.
-// Дополнительно реализовать вывод информации о том, что объект был создан или очищен в конструкторах и деструкторах, чтобы убедиться, что деревья продолжают существовать после удаления леса.
-
-// Традиционно принято считать, что композиция - это создание объекта внутри класса (по значению). Агрегация - это создание объекта вне класса. При композиции объект класса не может существовать без класса-контейнера. При агрегации - может. 
-// При каждом из этих способов, помимо создания объектов по значению, можно создавать их динамически, что будет являться более оптимальным способом работы с памятью.
-// Композиция и агрегация почти идентичны. При композиции с созданием по значению происходит копирование объекта в поле класса. Агрегация по указателю работает напрямую с памятью, и создает объект прямо в классе. 
-// Композиция по значению позволяет иметь два объекта (изначально созданный и скопированный), а агрегация по указателю один. Первый вариант отношения классов стоит применять, когда существует необходимость прослеживать изменения, который совершились с объектом класса после присоединения его в класс-контейнер.
-
-//      Элементарными словами
-// Композиция - создание объекта внутри класса. Агрегация - создание вне класса. У каждого из них может быть работа с указателями и со значениями.
-
-// Пример по значению для композиции: 
-//class Person
-//{
-//private:
-//    Leg leg;
-//};
-//
-//class Leg {};
-
-// Пример с указателем для композиции:
-//class Computer
-//{
-//public:
-//    Computer(std::string gpuModelName, std::string cpuModelName, std::string motherModelName)
-//    {
-//        gpu = new GPU(gpuModelName);
-//        cpu = new CPU(cpuModelName);
-//        motherBoard = new MotherBoard(motherModelName);
-//    }
-//    void StartComputer()
-//    {
-//        gpu->StartGPU();
-//        cpu->StartCPU();
-//        motherBoard->StartMotherBoard();
-//    }
-//    void PrintSpecifications()
-//    {
-//        gpu->PrintInfo();
-//        cpu->PrintInfo();
-//        motherBoard->PrintInfo();
-//    }
-//private:
-//    GPU* gpu; // видеокарта
-//    CPU* cpu; // процессор
-//    MotherBoard* motherBoard; // материнская плата
-//};
-
-// Пример по значению для агрегации:
-//class Computer
-//{
-//public:
-//    Computer(GPU gpu, CPU cpu, MotherBoard motherBoard)
-//    {
-//        this->gpu = gpu;
-//        this->cpu = cpu;
-//        this->motherBoard = motherBoard;
-//    }
-//    void StartComputer()
-//    {
-//        gpu.StartGPU();
-//        cpu.StartCPU();
-//        motherBoard.StartMotherBoard();
-//    }
-//    void PrintSpecifications()
-//    {
-//        gpu.PrintInfo();
-//        cpu.PrintInfo();
-//        motherBoard.PrintInfo();
-//    }
-//private:
-//    GPU gpu; // видеокарта
-//    CPU cpu; // процессор
-//    MotherBoard motherBoard; // материнская плата
-//};
-//
-//int main()
-//{
-//    setlocale(LC_ALL, "Ru");
-//    SetConsoleCP(1251);
-//    SetConsoleOutputCP(1251);
-//
-//    GPU gpu("GTX 1080");
-//    CPU cpu;
-//    MotherBoard motherBoard;
-//    Computer computer(gpu, cpu, motherBoard);
-//    computer.StartComputer();
-//    std::cout << "========" << std::endl;
-//    computer.PrintSpecifications();
-//
-//    system("pause");
-//}
-
-// Пример с указателем для агрегации:
-//int main()
-//{
-//    setlocale(LC_ALL, "Ru");
-//    SetConsoleCP(1251);
-//    SetConsoleOutputCP(1251);
-//
-//    GPU* gpu = new GPU("GTX 1080");
-//    CPU* cpu = new CPU();
-//    MotherBoard* motherBoard = new MotherBoard();
-//    Computer computer(gpu, cpu, motherBoard);
-//    computer.StartComputer();
-//    std::cout << "========" << std::endl;
-//    computer.PrintSpecifications();
-//
-//    system("pause");
-//}
+//      Практика
+// 1. Переделать возврат DoSomething() у PartComponent, например, на "Дело сделано."
+// 2. Создать новый класс поведения DoWork, который наследуется от Component и переопределяет функцию DoSomething(). Возвращать значение "Опять работа."
+// 3. Продемонстрировать новый компонент в main()
+// 4. Создать новый класс HumanRacist, который наследуется от Decorator и во многом повторяет класс Minion, только выводит, что что-то сделал HumanRacist.
+// 5. Продемонстрировать новый декоратор в main()
