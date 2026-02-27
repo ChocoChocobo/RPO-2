a411952e8219f4c83c1bb06e6d406e80e39cd48c
ChocoChocobo <sharonovjr@yahoo.com>
2026-02-27 14:22:12 +0300

    27.02 Декоратор

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -2,57 +2,57 @@
 #include <Windows.h>
 #include <iostream>
 
-// Класс с чистыми виртуальными функциями (интерфейс). Определяет поведение, которое изменяется с помощью декоратора
-class Component
+class FlowerBoss
 {
 public:
-    virtual ~Component(){}
-    virtual std::string DoSomething() = 0;
-};
-
-// Класс, реализующий поведение по умолчанию. У этого класса может быть несколько вариаций
-class PartComponent : public Component
-{
-public:
-    std::string DoSomething() override
+    // Шаблонный метод
+    void GiveOrder() const
     {
-        return "Defualt PartComponent";
-    }
-};
-
-// Следует тому же интерфейсу, что и другие компоненты. Определяет обертку для всех конкретных декораторов
-class Decorator : public Component
-{
-public:
-    Decorator(Component* component)
-    {
-        this->component = component;
-    }
-    std::string DoSomething() override
-    {
-        return component->DoSomething();
+        this->CollectFlowers();
+        this->SellFlowers();
+        this->RequireCollectFlowers();
+        this->RequireSellFlowers();
+        this->RequirePackageFlowers();
     }
 protected:
-    Component* component;
+    void CollectFlowers() const
+    {
+        std::cout << "Цветочный босс говорит собрать цветы" << std::endl;
+    }
+    void SellFlowers() const
+    {
+        std::cout << "Цветочный босс говорит продать цветы" << std::endl;
+    }
+    virtual void RequireCollectFlowers() const {}
+    virtual void RequireSellFlowers() const {}
+    virtual void RequirePackageFlowers() const {}
 };
 
-// Конкретные декораторы вызывают обернутый объект и изменяют его результат.
-// Декораторы могут вызывать родительскую реализацию операции, вместо того, чтобы вызвать обернутый объект напрямую
-class Minion : public Decorator
+class PlatonFlower : public FlowerBoss
 {
-public:
-    Minion(Component* component) : Decorator(component) {}
-    std::string DoSomething() override
+protected:
+    void RequireCollectFlowers() const override
     {
-        // При наследовании обращение к родителю идет через оператор области видимости, поскольку поля с объектом класса в текущем классе нет.
-        return "Minion сделал: " + Decorator::DoSomething();
+        std::cout << "Платон слушает и повинуется собирать цветы" << std::endl;
+    }
+    void RequireSellFlowers() const override
+    {
+        std::cout << "Платон слушает и повинуется продавать цветы" << std::endl;
     }
 };
 
-// Функция, работающая  со всеми объектами, использующая интерфейс компонента.Функция независима от конкретных классов компонентов с которыми работает
-void GuideMinion(Component* component)
+class SirFlower : public FlowerBoss
 {
-    std::cout << "Результат работы миньона: " << component->DoSomething();
+protected:
+    void RequirePackageFlowers() const override
+    {
+        std::cout << "Сир цветок слушает и повинуется упаковывать цветы" << std::endl;
+    }
+};
+
+void ManageWorker(FlowerBoss* flowerBoss)
+{
+    flowerBoss->GiveOrder();
 }
 
 int main()
@@ -61,25 +61,12 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // Поддержка простых компонентов:
-    Component* componentA = new PartComponent;
-    GuideMinion(componentA);
-    std::cout << std::endl << "==================" << std::endl;
+    PlatonFlower* platonFlower = new PlatonFlower;
+    ManageWorker(platonFlower);
 
-    // Поддержка декоративных компонентов:
-    Component* decoratorA = new Minion(componentA);
-    GuideMinion(decoratorA);
-    std::cout << std::endl << "==================" << std::endl;
-
-    delete componentA;
-    delete decoratorA;
+    SirFlower* sirFlower = new SirFlower;
+    ManageWorker(sirFlower);
+    delete platonFlower;
 
     system("pause");
 }
-
-//      Практика
-// 1. Переделать возврат DoSomething() у PartComponent, например, на "Дело сделано."
-// 2. Создать новый класс поведения DoWork, который наследуется от Component и переопределяет функцию DoSomething(). Возвращать значение "Опять работа."
-// 3. Продемонстрировать новый компонент в main()
-// 4. Создать новый класс HumanRacist, который наследуется от Decorator и во многом повторяет класс Minion, только выводит, что что-то сделал HumanRacist.
-// 5. Продемонстрировать новый декоратор в main()
