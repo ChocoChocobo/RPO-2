322c7511c775cdce1d6280e9ea228d0b45b5b108
ChocoChocobo <sharonovjr@yahoo.com>
2026-03-06 13:13:32 +0300

    06.03 Наблюдатель

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -1,12 +1,100 @@
 ﻿#include <string>
 #include <Windows.h>
 #include <iostream>
+#include <list>
 
-// Функция, принимающая в качестве третьего параметра указатель на функцию, которая выступает в виде лямбда-выражения
-void DoSomething(int number1, int number2, int (*operation)(int, int))
+class IObserver // интерфейс для наблюдателя
 {
-    std::cout << operation(number1, number2) << std::endl;
-}
+public:
+    virtual ~IObserver() {};
+    virtual void Update(const std::string& message) = 0; // Функция, обновляющая поступающую информацию наблюдаемому
+};
+
+class ISubject // интерфейс для наблюдамого
+{
+public: 
+    virtual ~ISubject() {};
+    virtual void Subscribe(IObserver *observer) = 0; // Функция, подписывающая на наблюдателя
+    virtual void UnSubscribe(IObserver *observer) = 0; // Функция, отписывающая от наблюдателя
+    virtual void Notify() = 0; // Уведомить наблюдаемого
+};
+
+class Subject : public ISubject
+{
+public:
+    ~Subject()
+    {
+        std::cout << "Наблюдаемый был очищен в памяти" << std::endl;
+    }
+
+    // Методы управления подпиской
+    void Subscribe(IObserver* observer) override
+    {
+        observerList.push_back(observer);
+    }
+
+    void UnSubscribe(IObserver* observer) override
+    {
+        observerList.remove(observer);
+    }
+
+    void Notify() override
+    {
+        // Для того, чтобы определить свою единицу перечисления для такого типа данных, как список, создается итератор, а внего задается начальный объект списка
+        std::list<IObserver*>::iterator iterator = observerList.begin();
+        std::cout << "За объектом наблюдает" << observerList.size() << " наблюдателей" << std::endl;
+        // Пока значение итератора не примет вид последнего объекта из списка
+        while (iterator != observerList.end())
+        {
+            // Вызываем функцию Update у текущего перечисляемого наблюдателя
+            (*iterator)->Update(message);
+            ++iterator; // У объекта итератора перегружен оператор ++
+        }
+    }
+
+    // Функция, при вызове которой задается новое сообщение (новое состояние) для наблюдаемого и происходит автоматическое уведомление наблюдателя через функцию Notify()
+    void CreateMessage(std::string message)
+    {
+        this->message = message;
+        Notify();
+    }
+private:
+    std::list<IObserver*> observerList;
+    std::string message;
+};
+
+class Observer : public IObserver
+{
+public:
+    // Конструктор
+    Observer(Subject& _subject) : subject(_subject)
+    {
+        // Наблюдаемый в классе наблюдателя подписывается на второго сразу же в конструкторе
+        this->subject.Subscribe(this);
+        std::cout << "Наблюдатель проинициализирован" << std::endl;
+    }
+    // Деструктор
+    ~Observer()
+    {
+        std::cout << "Наблюдатель был очищен" << std::endl;
+    }
+    // Переопределенная Update
+    void Update(const std::string& message) override
+    {
+        this->message = message;
+        // Наблюдатель должен реагировать на поступаемое изменение
+        std::cout << "У наблюдателя появилось новое сообщение от наблюдаемого: " << this->message << std::endl;
+    }
+    // Вспомогательная функция по управлению количеством наблюдаемых
+    void UnsubscribeSubject()
+    {
+        subject.UnSubscribe(this);
+        std::cout << "Наблюдаемый был отписан от наблюдателя!" << std::endl;
+    }
+private:
+    Subject& subject;
+    std::string message;
+};
 
 int main()
 {
@@ -14,60 +102,30 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // Безымянная лямбда
-    []() {std::cout << "Привет от лямбды без имени" << std::endl; } ();
+    Subject* subjectLeon = new Subject;
+    Observer* observerGreka = new Observer(*subjectLeon);
+    Observer* observerRak = new Observer(*subjectLeon);
+    Observer* observerReka = new Observer(*subjectLeon);
 
-    // Именованная лямбда
-    auto hello{ []() {std::cout << "Привет от именованной лямбды" << std::endl; } };
+    subjectLeon->CreateMessage("Ты ЛОХнесское чудовище");
+    observerReka->UnsubscribeSubject();
 
-    // лямбда с параметрами
-    auto printInfo{ [](const std::string& info) {std::cout << info << std::endl; } };
+    subjectLeon->CreateMessage("Я люблю крабов");
+    observerRak->UnsubscribeSubject();
 
-    hello();
+    subjectLeon->CreateMessage("Трусы");
+    observerGreka->UnsubscribeSubject();
 
-    printInfo("Привет РПО");
-
-    // вызов при объявлении лямбды с параметрами
-    [](const std::string& info) {std::cout << info << std::endl; }("Пока от РПО");
-
-    // лямбда с параметрами
-    auto sum{ [](int number1, int number2) {return number1 + number2; } };
-    std::cout << sum(2, 2) << std::endl;
-
-    // лямбда сс явным возвращаемым значением
-    [](int number1, int number2) -> double {return number1 + number2; } (5, 5);
-
-    auto subtract{ [](int number1, int number2) {return number1 - number2; } };
-
-    DoSomething(10, 5, sum);
-    DoSomething(10, 5, subtract);
+    delete observerGreka;
+    delete observerRak;
+    delete observerReka;
+    delete subjectLeon;
 
     system("pause");
 }
 
-//      Безымянные лямбда-выражения
-// Представляют собой краткий компактный синтаксис для определения объектов-функций (не объект класса!).
-// Синтаксис объявления: [] (параметры) {действия}
-// Пример объявления: [] () {std::cout << "Привет РПО" << std::endl;};
-// Синтаксис вызова: [] (параметры) {действия} ()
-// Пример вызова: [] () {std::cout << "Привет РПО" << std::endl;} ();
-// Вызов происходит непосредственно при определении, указав после тела выражения круглые скобки со значениями для параметров
-
-//      Именованные лямбда-выражения
-// Лямбда-выражение можно определить как переменную.
-// Пример: auto hello{ []() {std::cout << "Привет РПО" << std::endl; } };
-// Такие выражения также можно определить с параметрами.
-// Пример: auto printInfo{ [](const std::string& info) {std::cout << info << std::endl; } };
-// Лямбда-выражение может возвращать какое-либо значение, как и обычные функции. 
-// Пример: auto sum{ [](int number1, int number2) {return number1 + number2; } };
-// Для того, чтобы установить напрямую возвращемое значение типа после списка параметров, указывается оператор стрелки и возвращаемый тип.
-
-// Лямбда-выражения могут использоваться как параметры для функций, которые представляют собой указатели на функции
-// Функция, принимающая в качестве третьего параметра указатель на функцию, которая выступает в виде лямбда-выражения
-//void DoSomething(int number1, int number2, int (*operation)(int, int))
-//{
-//    std::cout << operation(number1, number2) << std::endl;
-//}
-// На основе того, что передаетсся, можно делать с разным результатом работы лямбды что угодно. В примере выше разные арифметические операции 
-
-// Реализовать простой калькулятор с использованием лямбда-выражений, как в примере с лекции (операции +, -, *, /). В main получать ввод пользователя по выбору операции, в конструкции switch в зависимости от операции вызывать функцию Calculator, передавая в нее лямбда арифметическое лямбда выражение
+//      Практика
+// 1. Сделать статическую переменную подсчета кол-ва созданных наблюдателей. Продемонстрировать в конструкторе номер создаваемого объекта
+// 2. У наблюдателя создать поле с названием объекта, которое должно быть проинициализировано в констукторе. Выводить в консоль в строках у наблюдателя также его имя
+// 3. В main просить пользователя просить считать 3 уравнения по очерди. Если уравнение решено неверно, то от наблюдаемого должен отписываться один фанат. Если к концу мини-игры у пользователю не остается фанатом, выводить злостное сообщение и создавать, подписывая на наблюдаемого, модератора твича.
+// Прим.: У наблюдаемого должно быть новое поле с ответом, который дает пользователь в консоли. При поступлении нового сообщения наблюдателям, они должны проверять соответствует ли ответ правильным.
