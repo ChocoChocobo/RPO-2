21f02552ad589892eca9a444fd80d23e784278fd
ChocoChocobo <sharonovjr@yahoo.com>
2026-03-31 13:36:10 +0300

    31.03 dynamic_cast

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -2,32 +2,49 @@
 #include <Windows.h>
 #include <iostream>
 
+
+//-------------Интерфейс классов//-------------
+class Employee // Базовый класс для работника
+{
+public:
+    virtual int Salary();
+    virtual int Bonus();
+};
+
+class Manager : Employee // категория сотрудника менеджер
+{
+public:
+    int Salary();
+    int Bonus();
+};
+
+class Programmer : Employee // категория сотрудника программист
+{
+public:
+    int Salary();
+    int Bonus();
+};
+
+void Paycheck(Employee* employee) // внешняя функция, принимающая указатель на сотрудника и вызывающая функцию у указателя по выплате
+{
+    employee->Salary();
+}
+//--------------------------
+
+
 int main()
 {
     setlocale(LC_ALL, "Ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    std::shared_ptr<std::string> stringPointer; // nullptr
-
-    // Способ инициализаии с помощью специальной функции make_shared, предназначенной для этого
-    std::shared_ptr<float> floatPointer = {std::make_shared<float>(36.6)};
-
-    std::cout << stringPointer << std::endl;
-    // У shared_ptr есть функция, возвращающая количество объектов, на которые он ссылается
-    std::cout << floatPointer.use_count() << std::endl;
-
-    std::cout << *floatPointer << std::endl;
-
-    // Начиная с версии языка C++20 shared_ptr может указывать на массивы
-    std::shared_ptr<int[]> intArrayPointer = {std::make_shared<int[]>(10)};
-    std::cout << "Массив проинициализирован: ";
-    for (int i = 0; i < 10; i++)
-    {
-        intArrayPointer[i] = i * 2;
-        std::cout << " " << intArrayPointer[i] << " ";
-    }
-    std::cout << std::endl;
+    //  static_cast и dynamic_cast
+    // static_cast<тип_данных>(значение);
+    // statics_cast преобразует значение в круглых скобках - value к типу, который указан в угловых скобках
+    // dynamic_cast
+    float x = 5.5;
+    float y = 6.6;
+    int result = static_cast<int>(y / x);
 
     system("pause");
 }
