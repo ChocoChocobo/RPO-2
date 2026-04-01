a805368abd0d7e8338e54bdff4711d8eb2c29662
ChocoChocobo <sharonovjr@yahoo.com>
2026-04-01 15:20:06 +0300

    01.04 dynamic_cast на примере с классами животных

--- HelloStructs.cpp
+++ HelloStructs.cpp
@@ -1,21 +1,87 @@
 ﻿#include <string>
 #include <Windows.h>
 #include <iostream>
+#include <vector>
 
+class Animal
+{
+public:
+    virtual ~Animal() {}
+    virtual void MakeSounds() const
+    {
+        std::cout << "Какое-то животное издает какой-то звук" << std::endl;
+    }
+};
 
+class Cat : public Animal
+{
+public:
+    void MakeSounds() const override
+    {
+        std::cout << "Мяу-мяу!" << std::endl;
+    }
+};
+
+class Bird : public Animal
+{
+public:
+    void MakeSounds() const override
+    {
+        std::cout << "Чирик-чик!" << std::endl;
+    }
+
+    void Fly() const
+    {
+        std::cout << "Птица взлетела!" << std::endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::vector<std::unique_ptr<Animal>> zoo;
+    zoo.push_back(std::make_unique<Cat>());
+    zoo.push_back(std::make_unique<Bird>());
+
+    for (auto& animal : zoo)
+    {
+        animal->MakeSounds();
+
+        Bird* birdPointer = dynamic_cast<Bird*>(animal.get());
+        if (birdPointer)
+        {
+            birdPointer->Fly();
+        }
+        else
+        {
+            std::cout << "Перечисляемое животное летать не умеет!" << std::endl;
+        }
+    }
+
+    system("pause");
+}
+
+// Данный оператор применяется для получения указателя на наследуемый класс, чтобы иметь возможность работать с теми его элементами, которые по-другому не доступны.
+// На примере рассматриваемых классов можно представить следующую ситуацию: необходимо расширить функционал класса за счет добавления новой функции. Ее объявление можно включить в класс Programmer, находящееся в заголовочном файле, а саму функцию определить в одном из своих исходных файлов.
+// Для того, чтобы получить конкретный наследуемый объект класса используется оператор dynamic_cast, где в угловых скобках указывается указатель на тип данных, а в круглых - объект, от которого нужно получить указатель на тип данных
+// Programmer* programmer = dynamic_cast<Programmer*>(employee);
+// Таким образом, оператор dynamic_cast осуществляется две операции: проверяет выполнимо ли преобразование и если это так, выполняет его.
 //-------------Интерфейс классов//-------------
-class Employee // Базовый класс для работника
+/*class Employee // Базовый класс для работника
 {
 public:
     virtual int Salary();
-    virtual int Bonus();
+
 };
 
 class Manager : Employee // категория сотрудника менеджер
 {
 public:
     int Salary();
-    int Bonus();
+
 };
 
 class Programmer : Employee // категория сотрудника программист
@@ -28,23 +94,10 @@ public:
 void Paycheck(Employee* employee) // внешняя функция, принимающая указатель на сотрудника и вызывающая функцию у указателя по выплате
 {
     employee->Salary();
+    // Для того, чтобы получить конкретный наследуемый объект класса используется оператор dynamic_cast, где в угловых скобках указывается указатель на тип данных, а в круглых - объект, от которого нужно получить указатель на тип данных
+    Programmer* programmer = dynamic_cast<Programmer*>(employee);
 }
-//--------------------------
+//--------------------------*/
 
-
-int main()
-{
-    setlocale(LC_ALL, "Ru");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
-    //  static_cast и dynamic_cast
-    // static_cast<тип_данных>(значение);
-    // statics_cast преобразует значение в круглых скобках - value к типу, который указан в угловых скобках
-    // dynamic_cast
-    float x = 5.5;
-    float y = 6.6;
-    int result = static_cast<int>(y / x);
-
-    system("pause");
-}
+//      Практика
+// Отладить программу и выяснить почему с проверкой на наличие указателя у объекта кошки в массиве не вызывается функция Fly(), а с отсутсвием проверки функция вызывается.
